2ffd25c3bf6b7d94394704dcf52cec7a33c35391
ThomasGITH <31830553+ThomasGITH@users.noreply.github.com>
2018-11-26 11:13:50 +0100

    Initial commit, added model-rotation

---
+++ Assets/scripts/modelDrag.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modelDrag : MonoBehaviour
+{
+    private Vector3 rotation, mouseOffset, mouseReference;
+    public float sensitivity;
+    private bool mouseHold;
+
+    void Awake()
+    {
+        rotation = Vector3.zero;
+        sensitivity = sensitivity > 0.0f ? sensitivity : 0.4f;
+    }
+
+    void Update()
+    {
+        if (mouseHold)
+        {
+            mouseOffset = (Input.mousePosition - mouseReference);
+
+            rotation.y = -(mouseOffset.x + mouseOffset.y) * sensitivity;
+
+            transform.Rotate(rotation);
+            mouseReference = Input.mousePosition;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        mouseHold = true;
+        mouseReference = Input.mousePosition;
+    }
+
+    private void OnMouseUp()
+    {
+        mouseHold = false;
+    }
+
+}
